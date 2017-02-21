c7f87716ab01357a34fddb0a92ac3d6c9507703f
Grant Patterson <grantrpatterson@gmail.com>
2017-02-21 14:34:45 -0600

    working in browser

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2Environment(loader = jinja2.FileSystemLoader(template_dir),
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                               autoescape = True)
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params)
-        return render_str(template, **params)   #should trigger return(purple)
+    def render_str(self, template, **params):
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
@@ -130,7 +136,7 @@ class Signup(BaseHandler):
 class Welcome(BlogHandler):
     def get(self):
         username = self.request.get('username')
-        if valid_username(username);
+        if valid_username(username):
             self.render('welcome.html', username = username)
         else:
             self.redirect()
